89f6e3add215b9d8d54762893edc826831c62a14
Codefred <alfredemmanuelinyang@gmail.com>
2020-04-13 21:03:17 -0400

    Add transaction table

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+from sqlalchemy import Table
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, backref
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 
@@ -9,11 +10,16 @@ Base = declarative_base()
 engine = create_engine("sqlite:///database.db", echo=True)
 # Change encho to false before deploying
 
+# user_transaction_association = Table('user_transactions', Base.metadata,
+#     Column('user_id', Integer, ForeignKey('user.id')),
+#     Column('transaction_id', Integer, ForeignKey('transaction.id'))
+# )
+
 class User(Base):
     """
-    SqlAlchemy ORM for Bot Application
+    SqlAlchemy ORM User Model
     """
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -21,10 +27,26 @@ class User(Base):
     xrp_balance = Column(Integer)
     date_joined = Column(String)
     address = Column(String)
+    transaction = relationship("Transaction", backref="owner")
 
     def __repr__(self):
         return "<User(id='%d', name='%s')>" % (self.id, self.first_name)
 
+
+class Transaction(Base):
+    """
+    SqlAlchemy ORM Transaction Model
+    """
+    __tablename__ = 'transaction'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer)
+    date_created = Column(String(50))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    # user = relationship("User", back_populates="transaction")
+
+    def __repr__(self):
+        return "<Tansaction(id='%s')>" % (self.id)
 
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
