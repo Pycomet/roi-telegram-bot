5cba7b7607e44d72f10d0115a4c1d99b67764e80
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-05 05:29:07 +0100

    fixed complaint

--- config.py
+++ config.py
@@ -3,17 +3,18 @@ from flask import Flask, request
 import telebot
 from telebot import types
 import emoji
+from decouple import config
 
 # Bot token information
-TOKEN = '1027166109:AAH-VqLwJLRNfbPegg3hqFjdxrI6z0z4oGU'
+TOKEN = config("TOKEN")
 
 # Database Url
-DATABASE_URL = "postgres://ejjtgsuaguhtru:e8f8c4aec67b246f9315576c50e1306bfe8cdefb64520c5b44065f034b8e30c3@ec2-52-7-39-178.compute-1.amazonaws.com:5432/daokl2gmp4eef"
+DATABASE_URL = config("DATABASE_URL")
 
 # CoinBase API information
-API_KEY = "euAAnrDNp00ZhU5V"
+API_KEY = config("API_KEY")
 
-API_SECRET = "GjNojkpCtycOt7YahpXWndG6uv1oEoXr"
+API_SECRET = config("API_SECRET")
 
 
 # Refrenced wallet for investment splitting
